c36ecb32e75d2782ce7e670d836abedca2a936ac
Aleksandar Krsteski <aleksandar.krsteski@ved.mk>
2024-10-22 15:54:50 +0200

    Clean main

--- hooks/pre_gen_project.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    print()
-    print("PRE GEN PROJECT")
-    print()
--- {{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/api/ws/handlers/__init__.py
+++
@@ -1,3 +0,0 @@
-# from {{cookiecutter.module_name}}.api.ws.handlers import (
-#    # FIXME: Need this import for handler registration, try to find better way
-# )
