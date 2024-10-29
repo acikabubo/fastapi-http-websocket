c3776864d72869786fe4f5d9b1730232753142a7
Aleksandar Krsteski <aleksandar.krsteski@ved.mk>
2024-10-29 10:19:10 +0100

    Clean main branch

--- app/schemas/user.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserModel(BaseModel):
-    firstname: str
-    lastname: str
-    email: str
-    username: str
-    roles: list[str] = []
--- app/settings.py
+++
@@ -1,5 +0,0 @@
-from decouple import config
-
-ACTIONS_FILE_PATH = config(
-    "ACTIONS_FILE_PATH", cast=str, default="actions.json"
-)
