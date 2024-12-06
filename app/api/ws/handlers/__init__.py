1a9c843f8eaa306fce1b0ef1146ad361f7f48d4d
Aleksandar Krsteski <aleksandar.krsteski@ved.mk>
2024-12-06 15:27:47 +0100

    Dynamically loads all ws handlers in the handlers package to trigger the registration of handlers via decorators

--- app/api/ws/handlers/__init__.py
+++ app/api/ws/handlers/__init__.py
@@ -1,3 +1,15 @@
-from app.api.ws.handlers import (
-    author_handler,  # FIXME: Need this import for handler registration, try to find better way
-)
+import importlib
+import pkgutil
+
+
+def load_handlers():
+    """
+    Dynamically loads all ws handlers in the handlers package to
+    trigger the registration of handlers via decorators.
+    """
+    for _, module_name, _ in pkgutil.iter_modules(__path__):
+        importlib.import_module(f"{__name__}.{module_name}")
+
+
+# Load handlers at import time
+load_handlers()
